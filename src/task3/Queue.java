04c84832109d8e1bbf1bd443fa2a90784ca49970
amerharb <amer.harb@gmail.com>
2015-12-13 23:33:01 +0100

    add error message with undre and over flow add isEmpty add clear

--- src/task3/Queue.java
+++ src/task3/Queue.java
@@ -7,15 +7,29 @@ public class Queue<E>
 
     private Stack<E> inbox = new Stack<E>();
     private Stack<E> outbox = new Stack<E>();
-    
-    public void queue(E item)
+    private int limit;
+
+    public Queue()
     {
+        limit = Integer.MAX_VALUE;
+    }
+
+    public Queue(int limit)
+    {
+        this.limit = limit;
+    }
+
+    public void queue(E item) throws Exception
+    {
+        if (size() >= limit) {
+            throw new Exception("over flow");
+        }
         inbox.push(item);
     }
 
     public E dequeue() throws Exception
     {
-        if (size() <= 0){
+        if (size() <= 0) {
             throw new Exception("under flow");
         }
         if (outbox.isEmpty()) {
@@ -25,8 +39,9 @@ public class Queue<E>
         }
         return outbox.pop();
     }
-    
-    public int size(){
+
+    public int size()
+    {
         return inbox.size() + outbox.size();
     }
 
@@ -40,4 +55,12 @@ public class Queue<E>
         return outbox.peek();
     }
 
+    public boolean isEmpty(){
+        return inbox.isEmpty() & outbox.isEmpty();
+    }
+    
+    public void clear(){
+        inbox.clear();
+        outbox.clear();
+    }
 }
